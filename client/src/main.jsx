55c3b3eef9a2d70cc164cf49109de4dd1e4ce0ec
Amol Rautrao <amolrautrao23@gmail.com>
2025-08-21 10:17:55 +0530

    removed old client part and add new

--- client/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App'; // App component
-import './styles/global.css'; // Global CSS styles
-import 'tailwindcss/tailwind.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-      <App />
-);
